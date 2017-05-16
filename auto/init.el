3c637c58ed36cef32d628501339423dd17be52ab
Stackdatcamp <stackdatcamp@gmail>
2017-05-17 06:07:57 +0700

    Initial commit

---
+++ auto/init.el
@@ -0,0 +1,47 @@
+(TeX-add-style-hook
+ "init"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("vietnam" "utf8")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "vietnam")
+   (LaTeX-add-labels
+    "sec:org7ed0e9f"
+    "sec:orgb390509"
+    "sec:org351e26a"
+    "sec:orge2fe01b"
+    "sec:org5b8b177"
+    "sec:org44f4a26"
+    "sec:org1fd1df5"
+    "sec:org9884e1d"
+    "sec:orge3330f6"
+    "sec:orgcb5d600"
+    "sec:orgf6efb0b"
+    "sec:orgce27dc9"))
+ :latex)
+
