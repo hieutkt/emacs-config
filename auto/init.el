b47dba6c42307a590ec159e866c8e39db485cd25
Stackdatcamp <stackdatcamp@gmail>
2017-05-30 22:39:58 +0700

    Add "Magic TAB"

--- auto/init.el
+++ auto/init.el
@@ -5,17 +5,17 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("vietnam" "utf8")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -35,30 +35,31 @@
     "hyperref"
     "vietnam")
    (LaTeX-add-labels
-    "sec:orgf72aa21"
-    "sec:orgda3613a"
-    "sec:orgdf484df"
-    "sec:org041d096"
-    "sec:org8622ded"
-    "sec:org8d90c99"
-    "sec:orge7fd6ce"
-    "sec:org32abe88"
-    "sec:org4261dd5"
-    "sec:org3bed63b"
-    "sec:org3615f16"
-    "sec:org1c58642"
-    "sec:org5531610"
-    "sec:org15548c5"
-    "sec:org5f9a376"
-    "sec:org2700791"
-    "sec:org876840b"
-    "sec:orgeba9eab"
-    "sec:orgf97159f"
-    "sec:org310c788"
-    "sec:orgb20ca7b"
-    "sec:org4a654b2"
-    "sec:org88b2556"
-    "sec:org6c80e8c"
-    "sec:org5646bf8"))
+    "sec:org3ef80d1"
+    "sec:org9fb20ef"
+    "sec:org02182ad"
+    "sec:orgc3ea522"
+    "sec:org271f9f0"
+    "sec:orgc03d443"
+    "sec:org7e571a2"
+    "sec:org035855f"
+    "sec:orgbbcdd10"
+    "sec:orgdcd692d"
+    "sec:org39877f3"
+    "sec:orge205089"
+    "sec:orgcc31bf0"
+    "sec:orga2d7370"
+    "sec:orgc7cf1c0"
+    "sec:orgf1b1384"
+    "sec:org496447f"
+    "sec:org6ada814"
+    "sec:org047d50a"
+    "sec:org07a031f"
+    "sec:org304f1a9"
+    "sec:orge77f68c"
+    "sec:org752ba4d"
+    "sec:orgb2f277f"
+    "sec:orgf57b66a"
+    "sec:org9415def"))
  :latex)
 
--- init.el
+++ init.el
@@ -153,10 +153,31 @@
   (find-file (format "%s/%s" config-directory "init.org"))))
 
 ;; Enable Yasnippets
-(require 'yasnippet)
-(yas-global-mode 1)
+  (require 'yasnippet)
+  (add-to-list 'yas-snippet-dirs (format "%s/%s" config-directory "Snippets"))
 
-(setq yas-snippet-dirs (format "%s/%s" config-directory "Snippets"))
+  (yas-global-mode 1)
+  
+  (global-set-key (kbd "<C-tab>") 'yas-insert-snippet)
+
+
+  ;; It will test whether it can expand, if yes, cursor color -> green.
+(defun yasnippet-can-fire-p (&optional field)
+  (interactive)
+  (setq yas--condition-cache-timestamp (current-time))
+  (let (templates-and-pos)
+    (unless (and yas-expand-only-for-last-commands
+                 (not (member last-command yas-expand-only-for-last-commands)))
+      (setq templates-and-pos (if field
+                                  (save-restriction
+                                    (narrow-to-region (yas--field-start field)
+                                                      (yas--field-end field))
+                                    (yas--templates-for-key-at-point))
+                                (yas--templates-for-key-at-point))))
+
+  (set-cursor-color (if (and templates-and-pos (first templates-and-pos)) 
+                        "green" "white"))))
+  (add-hook 'post-command-hook 'yasnippet-can-fire-p)
 
 (require 'helm)
 (require 'helm-config)
@@ -216,6 +237,78 @@ helm-echo-input-in-header-line        t)
 (add-to-list 'auto-mode-alist '("\\.Rbrew$" . poly-brew+r-mode))
 (add-to-list 'auto-mode-alist '("\\.Rcpp$" . poly-r+c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cppR$" . poly-c++r-mode))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+    (backward-char 1)
+    (if (looking-at "->") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (cond
+   ((minibufferp)
+    (minibuffer-complete))
+   (t
+    (indent-for-tab-command)
+    (if (or (not yas/minor-mode)
+        (null (do-yas-expand)))
+    (if (check-expansion)
+        (progn
+          (company-manual-begin)
+          (if (null company-candidates)
+          (progn
+            (company-abort)
+            (indent-for-tab-command)))))))))
+
+(defun tab-complete-or-next-field ()
+  (interactive)
+  (if (or (not yas/minor-mode)
+      (null (do-yas-expand)))
+      (if company-candidates
+      (company-complete-selection)
+    (if (check-expansion)
+      (progn
+        (company-manual-begin)
+        (if (null company-candidates)
+        (progn
+          (company-abort)
+          (yas-next-field))))
+      (yas-next-field)))))
+
+(defun expand-snippet-or-complete-selection ()
+  (interactive)
+  (if (or (not yas/minor-mode)
+      (null (do-yas-expand))
+      (company-abort))
+      (company-complete-selection)))
+
+(defun abort-company-or-yas ()
+  (interactive)
+  (if (null company-candidates)
+      (yas-abort-snippet)
+    (company-abort)))
+
+(global-set-key [tab] 'tab-indent-or-complete)
+(global-set-key (kbd "TAB") 'tab-indent-or-complete)
+(global-set-key [(control return)] 'company-complete-common)
+
+(define-key company-active-map [tab] 'expand-snippet-or-complete-selection)
+(define-key company-active-map (kbd "TAB") 'expand-snippet-or-complete-selection)
+
+(define-key yas-minor-mode-map [tab] nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+
+(define-key yas-keymap [tab] 'tab-complete-or-next-field)
+(define-key yas-keymap (kbd "TAB") 'tab-complete-or-next-field)
+(define-key yas-keymap [(control tab)] 'yas-next-field)
+(define-key yas-keymap (kbd "C-g") 'abort-company-or-yas)
 
 ;; Enable shift selection
 (setq org-support-shift-select t)
